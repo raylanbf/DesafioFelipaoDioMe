377dc4c610db734304dd084bc23c1888d5fc4ef2
Raylan B. Fraga <47982526+raylanbf@users.noreply.github.com>
2025-12-30 12:34:45 -0300

    Add hero level determination based on experience points

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+const xp = 1000;
+const nomeHeroi = "Gustavo";
+let nivelDeHeroi;
+
+switch (true) {
+  case xp < 1000:
+    nivelDeHeroi = "Ferro";
+    break;
+
+  case xp <= 2000:
+    nivelDeHeroi = "Bronze";
+    break;
+
+  case xp  <= 5000:
+    nivelDeHeroi = "Prata";
+    break;
+
+  case xp <= 7000:
+    nivelDeHeroi = "Ouro";
+    break;
+
+  case xp  <= 8000:
+    nivelDeHeroi = "Platina";
+    break;
+   
+  case xp <= 9000:
+    nivelDeHeroi = "Ascendente";
+    break;  
+    
+ case xp <= 10000:
+    nivelDeHeroi = "Imortal";
+    break;  
+
+  default:
+    nivelDeHeroi = "Radiante";
+}
+
+console.log(
+  "O Herói de nome " + nomeHeroi + " está no nível " +  nivelDeHeroi +
+  " com " + xp + " xp"
+);
